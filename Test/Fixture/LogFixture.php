4f55e27c7e80cc98dd52fe5fecd8455c8bbba33c
Nick Baker <nick@webtechnick.com>
2012-09-12 19:54:05 -0600

    Adding test fixture

---
+++ Test/Fixture/LogFixture.php
@@ -0,0 +1,23 @@
+<?php
+/* Log Fixture generated on: 2011-08-08 13:46:28 : 1312832788 */
+class LogFixture extends CakeTestFixture {
+	var $name = 'Log';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'message' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'type' => 'Lorem ipsum dolor sit amet',
+			'message' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2011-08-08 13:46:28'
+		),
+	);
+}
