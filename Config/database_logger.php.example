95400dadd7337f00a0865619bec4b19b60c4385f
Nick Baker <nick@webtechnick.com>
2013-03-27 13:12:27 -0600

    New configs to read and write to different datasources if needbe.

---
+++ Config/database_logger.php.example
@@ -0,0 +1,12 @@
+<?php
+/**
+  * DatabaseLogger Configuration file
+  *
+  * save the file to app/Config/database_logger.php
+  */
+$config = array(
+  'DatabaseLogger' => array(
+    'write' => 'default', //DataSource to write to.
+    'read' => 'default', //Dadtasource to read from.
+  )
+);
--- Model/DatabaseLoggerAppModel.php
+++ Model/DatabaseLoggerAppModel.php
@@ -7,6 +7,45 @@ class DatabaseLoggerAppModel extends AppModel {
 	var $searchFields = array();
 	
 	/**
+	* Configurations
+	*/
+	public $configs = array(
+		'write' => 'default',
+		'read' => 'default',
+	);
+	
+	/**
+	* Set the default datasource to the read setup in config
+	*/
+	public function __construct($id = false, $table = null, $ds = null) {
+		if(Configure::load('database_logger')){
+			$this->configs = Configure::read('DatabaseLogger');
+		}
+		parent::__construct($id, $table, $ds);
+		$this->setDataSource($this->configs['read']);
+	}
+	
+	/**
+	* Overwrite save to write to the datasource defined in config
+	*/
+	public function save($data = null, $validate = true, $fieldList = array()) {
+		$this->setDataSource($this->configs['write']);
+		$retval = parent::save($data, $validate, $fieldList);
+		$this->setDataSource($this->configs['read']);
+		return $retval;
+	}
+	
+	/**
+	* Overwrite delete to delete to the datasource defined in config
+	*/
+	public function delete($id = null, $cascade = true) {
+		$this->setDataSource($this->configs['write']);
+		$retval = parent::delete($id, $cascade);
+		$this->setDataSource($this->configs['read']);
+		return $retval;
+	}
+	
+	/**
 	* Overwrite find so I can do some nice things with it.
 	* @param string find type
 	* - last : find last record by created date
@@ -40,4 +79,3 @@ class DatabaseLoggerAppModel extends AppModel {
 		return $retval;
 	}
 }
-?>
