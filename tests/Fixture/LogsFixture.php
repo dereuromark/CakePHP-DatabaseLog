ff51d3c1a36450cdcb1bfa95594ef240162374ea
Mark Scherer <euromark@web.de>
2016-04-12 00:52:40 +0200

    Adjust fixture

--- tests/Fixture/LogsFixture.php
+++ tests/Fixture/LogsFixture.php
@@ -25,14 +25,13 @@ class LogsFixture extends TestFixture {
 		'id' => ['type' => 'integer', 'null' => false, 'default' => null],
 		'type' => ['type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'message' => ['type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
-		'context' => ['type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
+		'context' => ['type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'created' => ['type' => 'datetime', 'null' => true, 'default' => null],
 		'ip' => ['type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'hostname' => ['type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'uri' => ['type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'refer' => ['type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
 		'user_agent' => ['type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'],
-		'number' => ['type' => 'integer', 'null' => false, 'default' => 0],
 		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]],
 		'_options' => ['charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM']
 	];
@@ -46,6 +45,7 @@ class LogsFixture extends TestFixture {
 		[
 			'id' => 1,
 			'type' => 'Lorem ipsum dolor sit amet',
+			'context' => '',
 			'message' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
 			'created' => '2011-08-08 13:46:28'
 		],
