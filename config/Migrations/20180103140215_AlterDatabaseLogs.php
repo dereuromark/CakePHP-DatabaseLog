b50c8895ee349d029e8dda03e5d4b34b4f907bb8
Andrey Vystavkin <jujav4ik@gmail.com>
2018-01-03 16:08:15 +0200

    Alterting URI column to TEXT type
    JavaScript DataTables plugin uses GET request by default
    to perform AJAX calls for loading table content. In certain
    occassions it started hitting VARCHAR(255) column limits.
    
    As a quickfix, we can alter uri column into text, that will let
    storing longer URI's in the table.

---
+++ config/Migrations/20180103140215_AlterDatabaseLogs.php
@@ -0,0 +1,22 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AlterDatabaseLogs extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('database_logs');
+		$table->changeColumn('uri', 'text', [
+			'default' => null,
+			'null' => true,
+		]);
+        $table->update();
+    }
+}
