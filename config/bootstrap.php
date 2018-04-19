1e408a96dadda6d9a1a7c45f229b7274d43b38ec
mscherer <mark.scherer@spryker.com>
2018-04-19 18:07:49 +0200

    Fix deprecations

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,13 +4,13 @@ use Cake\Core\Configure;
 use Cake\Datasource\ConnectionManager;
 use Cake\Network\Exception\InternalErrorException;
 
-$hasDatabaseLogConfig = ConnectionManager::config('database_log');
+$hasDatabaseLogConfig = ConnectionManager::getConfig('database_log');
 if (!$hasDatabaseLogConfig && !in_array('sqlite', PDO::getAvailableDrivers())) {
 	throw new InternalErrorException('You need to either install pdo_sqlite, ' .
 		'or define the "database_log" connection name.');
 }
 if (!$hasDatabaseLogConfig) {
-	ConnectionManager::config('database_log', [
+	ConnectionManager::setConfig('database_log', [
 		'className' => 'Cake\Database\Connection',
 		'driver' => 'Cake\Database\Driver\Sqlite',
 		'database' => LOGS . 'database_log.sqlite',
