9f8bb348991199a271235fe6bc58cd21152682a4
mscherer <mark.scherer@spryker.com>
2018-04-28 16:39:20 +0200

    Fix CS.

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,7 @@
 <?php
+use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
-use Cake\Routing\RouteBuilder;
 
 Router::prefix('admin', function (RouteBuilder $routes) {
 	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
