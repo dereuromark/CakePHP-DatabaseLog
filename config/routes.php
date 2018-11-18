437441d64c14078acefadfbba084770ff7059ebc
mscherer <mark.scherer@spryker.com>
2018-11-18 23:53:41 +0100

    Simplify routes.

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,9 @@
 <?php
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Cake\Routing\Route\DashedRoute;
 
 Router::prefix('admin', function (RouteBuilder $routes) {
 	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
-		$routes->fallbacks(DashedRoute::class);
+		$routes->fallbacks();
 	});
 });
--- tests/config/routes.php
+++ tests/config/routes.php
@@ -4,10 +4,9 @@ namespace DatabaseLog\Test\App\Config;
 
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Cake\Routing\Route\DashedRoute;
 
 Router::prefix('admin', function (RouteBuilder $routes) {
-	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function ($routes) {
-		$routes->fallbacks(DashedRoute::class);
+	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
+		$routes->fallbacks();
 	});
 });
