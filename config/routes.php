bd6e240e49e994a75316e593ccd8b70001477d71
mscherer <mark.scherer@spryker.com>
2020-04-24 00:16:59 +0200

    Fix inflection

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::prefix('admin', function (RouteBuilder $routes) {
+Router::prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
 		$routes->connect('/', ['controller' => 'DatabaseLog', 'action' => 'index']);
 
--- tests/config/routes.php
+++ tests/config/routes.php
@@ -5,7 +5,7 @@ namespace DatabaseLog\Test\App\Config;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::prefix('admin', function (RouteBuilder $routes) {
+Router::prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
 		$routes->fallbacks();
 	});
