ef958a4180d3c304fd14bf4de67bf2490dd4d9c9
mscherer <mark.scherer@spryker.com>
2021-11-12 09:40:56 +0100

    Fix up routes file.

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,11 @@
 <?php
+/**
+ * @var \Cake\Routing\RouteBuilder $routes
+ */
 
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
+$routes->prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('DatabaseLog', ['path' => '/database-log'], function (RouteBuilder $routes) {
 		$routes->connect('/', ['controller' => 'DatabaseLog', 'action' => 'index']);
 
